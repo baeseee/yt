9cf414509694510973cf2e903386d52187eabd30
suna <49289656+baeseee@users.noreply.github.com>
2024-07-05 11:58:14 +0900

    Update data.js

--- data/data.js
+++ data/data.js
@@ -1,4 +1,7 @@
+/* 고유한 vid를 가져야 됨 */
 var datas = [
-    { singer: "IU", title: "너랑나", vid: "63MygeaWptc" },
-    { singer: "IU", title: "블루밍", vid: "D1PvIWdJ8xo" }
+    { singer: "IU", title: "너랑나", vid: "63MygeaWptc", poster: "너랑나.webp", type: "무대" },
+	{ singer: "IU", title: "라일락", vid: "9ISBfiu0fvM", poster: "라일락.webp", type: "무대" },
+	{ singer: "에스파", title: "수퍼노바", vid: "phuiiNCxRMg", poster: "수퍼노바.webp", type: "mv" },
+    { singer: "IU", title: "블루밍", vid: "D1PvIWdJ8xo", poster: "블루밍.webp", type: "mv" }
 ];
