8bc439ee2d3329e1b4f17fddf383d14c62ed11bc
suna <49289656+baeseee@users.noreply.github.com>
2025-04-05 20:24:13 +0900

    Create test.js

---
+++ data/test.js
@@ -0,0 +1,5 @@
+const data = [
+  { id: 1, name: "Alice", age: 25, job: "Engineer" },
+  { id: 2, name: "Bob", age: 30, job: "Designer" },
+  { id: 3, name: "Charlie", age: 28, job: "Developer" }
+];
