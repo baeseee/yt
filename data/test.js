99a591d57a168417e1669306760b8db259e8b773
suna <49289656+baeseee@users.noreply.github.com>
2025-04-05 20:33:32 +0900

    Update test.js

--- data/test.js
+++ data/test.js
@@ -1,5 +1,7 @@
 const data = [
-  { id: 1, name: "Alice", age: 25, job: "Engineer" },
-  { id: 2, name: "Bob", age: 30, job: "Designer" },
-  { id: 3, name: "Charlie", age: 28, job: "Developer" }
+  { col1: "월세", col2: 420000, col3: "O" },
+  { col1: "대출", col2: 265000, col3: "O" },
+  { col1: "네이버플러스", col2: 4900, col3: "X" },
+  { col1: "마블매일매일", col2: 4900, col3: "X" },
+  { col1: "건강보험", col2: 22000, col3: "O" }
 ];
