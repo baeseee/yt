f719b4a9cba271169d766e1c72545a30d54261d3
suna <49289656+baeseee@users.noreply.github.com>
2025-06-01 02:10:15 +0900

    Update test.js

--- data/test.js
+++ data/test.js
@@ -3,5 +3,5 @@ const data = [
   { col1: "대출", col2: 265000, col3: "O" },
   { col1: "네이버플러스", col2: 4900, col3: "X" },
   { col1: "마블매일매일", col2: 4900, col3: "X" },
-  { col1: "건강보험", col2: 22000, col3: "O" }
+  { col1: "적금", col2: 100000, col3: "O" }
 ];
